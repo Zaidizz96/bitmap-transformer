9da6efe87f71ab9c044960db95959eaae1b82259
Zaid Izzeldden <Zaid.ali.izziddine@gmail.com>
2023-08-15 23:05:29 +0300

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # bitmap-transformer
+This Java application allows you to transform bitmap images by converting them to different colors. The application provides command-line options to perform transformations and save the modified images.
+
+---
+### Contributors
+
+---
+- Zaid Izziddine
+- Marwa Hussein
+
+---
+### Run the application
+
+---
+this application can be running by take an input arguments from CLI.
+
+- First arguments from input CLI representing the bmp file path , that we want modify on it ex.`blackbuck/bmp`.
+- Second argument from  CLI representing the output new bmp file path ex. `newBlackBuck/bmp`.
+- Third argument from CLI representing the name of the transformation that we need.
+
+#### CLI command to run the app ( convert color to GREEN).
+
+``` bash
+./gradlew run --args 'assets/blackbuck.bmp assets/newBlackBuck.bmp green' 
+```
+---
+### Supported color transformations
+
+---
+
+- #### Green Conversion
+  Converts the image to shades of green while preserving the original red and blue components.
+![](.././bitmap-transformer/app/assets/newBlackBuck1.bmp)
+
+- #### Blue Conversion 
+  Converts the image to shades of blue while preserving the original red and green components.
+![](.././bitmap-transformer/app/assets/newBlackBuck.bmp)
--- app/src/main/java/bitmap/transformer/Bitmap.java
+++ app/src/main/java/bitmap/transformer/Bitmap.java
@@ -10,20 +10,20 @@ public class Bitmap
 {
     public BufferedImage pixelsBitMap;
 
-    public Bitmap(final String file) throws IOException {
-        final File fileName = new File(file);
+    public Bitmap( String file) throws IOException {
+         File fileName = new File(file);
         this.pixelsBitMap = ImageIO.read(fileName);
     }
 
     public void convertImageToBlue() {
         for (int row = 0; row < this.pixelsBitMap.getWidth(); ++row) {
             for (int col = 0; col < this.pixelsBitMap.getHeight(); ++col) {
-                final Color color = new Color(this.pixelsBitMap.getRGB(row, col));
-                final int r = color.getRed();
-                final int g = color.getGreen();
-                final int b = color.getBlue();
-                final int a = color.getAlpha();
-                final Color convertBlue = new Color(0, 0, b, a);
+                 Color color = new Color(this.pixelsBitMap.getRGB(row, col));
+                 int r = color.getRed();
+                 int g = color.getGreen();
+                 int b = color.getBlue();
+                 int a = color.getAlpha();
+                 Color convertBlue = new Color(0, 0, b, a);
                 this.pixelsBitMap.setRGB(row, col, convertBlue.getRGB());
             }
         }
@@ -33,20 +33,20 @@ public class Bitmap
     public void convertToGreen() {
         for (int row = 0; row < this.pixelsBitMap.getWidth(); ++row) {
             for (int col = 0; col < this.pixelsBitMap.getHeight(); ++col) {
-                final Color color = new Color(this.pixelsBitMap.getRGB(row, col));
-                final int r = color.getRed();
-                final int g = color.getGreen();
-                final int b = color.getBlue();
-                final int a = color.getAlpha();
-                final Color lavenderColor = new Color(0, g, 0, a);
+                 Color color = new Color(this.pixelsBitMap.getRGB(row, col));
+                 int r = color.getRed();
+                 int g = color.getGreen();
+                 int b = color.getBlue();
+                 int a = color.getAlpha();
+                 Color lavenderColor = new Color(0, g, 0, a);
                 this.pixelsBitMap.setRGB(row, col, lavenderColor.getRGB());
             }
         }
         System.out.println("image converted succesfully");
     }
 
-    public void writeImage(final String fileName) throws IOException {
-        final File fileToWrite = new File(fileName);
+    public void writeImage( String fileName) throws IOException {
+         File fileToWrite = new File(fileName);
         ImageIO.write(this.pixelsBitMap, "bmp", fileToWrite);
     }
 }
